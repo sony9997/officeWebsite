5d31908c249d5369c77009915e9d386586302db4
sony9997 <sony9997@sina.com>
2024-12-06 18:10:09 +0800

    增加页尾、api文档

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,55 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://qimuinfo.top',
 	integrations: [
 		starlight({
+			plugins: [
+				starlightOpenAPI([
+				  {
+					base: 'api/runway',
+					label: 'Runway API',
+					schema:
+					  './src/content/schemas/api-service.yaml',
+				  },
+				]),
+			],
 			title: 'QimuInfo',
+			description: '官网',
+			logo: {
+				src: './public/favicon.svg'
+			},
 			social: {
 				github: 'https://github.com/sony9997/officeWebsite',
+				'x.com': 'https://x.com/donghe71627',
+				instagram: 'https://www.instagram.com/sony999774/'
 			},
 			sidebar: [
 				{
-					label: 'Introduce',
+					label: '公司介绍',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'introduce' },
+						{ label: '公司介绍', slug: 'introduce' },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: '服务',
+					autogenerate: { directory: 'services' },
+				},
+				{ 
+					label: 'API文档',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						...openAPISidebarGroups,
+					]
 				},
 			],
+			components: {
+				Footer: './src/components/Footer.astro',
+			},
 		}),
 	],
 });
